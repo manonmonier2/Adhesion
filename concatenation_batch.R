333fb0c77046ab64add75235ec967a834f7efe6a
Jean-Noël <jeannoel.lorenzi@gmail.com>
2022-12-05 15:44:03 +0100

    Update concatenation_batch.R

--- concatenation_batch.R
+++ concatenation_batch.R
@@ -8,18 +8,22 @@ library("config")
 opt = config::get(file = paste0(dirname(rstudioapi::getSourceEditorContext()$path), "/config.yml"), config = "default")
 
 # retrieve parameters
+# Input
 path_data = opt$batches
+
+# Output
 path_output_file_data = opt$concatenate_file
 path_output_file_id = opt$concatenate_id_file
+
+#### MAIN
 
 # get all batch file (.xlsx file)
 list_infile = list.files(path_data, pattern = ".xlsx$", full.names = T)
 
 nb_row = 500 # number of measurements (ie line) expected for each pupa
 
-merge_pupe_data = ''
+merge_pupe_data = list()
 merge_id = c()
-merge_comment = c()
 
 for (infile in list_infile){
   
@@ -35,35 +39,25 @@ for (infile in list_infile){
     print(paste0("Pb in: ", infile, " - file ignored"))
     next
   }
-  
-  pupes_data = ''
+  pupes_data = list()
   for (i in seq(1, ncol(sheet1), by = 3)) {
     temp = sheet1[, c(i,(i+1), (i+2))]
     colnames(temp) = c("Time", "Load", "Extension")
     while(nrow(temp) < nb_row){
       temp = rbind(temp, rep(NA, 3))
     }
-    if (pupes_data == ''){
-      pupes_data = temp
-    } else {
-      pupes_data = cbind(pupes_data, temp)
-    }
+    pupes_data = c(pupes_data, list(temp))
   }
-  
-  if (merge_pupe_data == '') {
-    merge_pupe_data = pupes_data
-  }else {
-    merge_pupe_data = cbind(merge_pupe_data, pupes_data)
-  }
-  
+  pupes_data = as.data.frame(pupes_data)
+  merge_pupe_data = c(merge_pupe_data, list(pupes_data))
   merge_id = c(merge_id, list_id)
-  merge_comment = c(merge_comment, list_comment)
 }
+
+merge_pupe_data = as.data.frame(merge_pupe_data)
 
 colnames(merge_pupe_data) = rep(c("Time", "Load", "Extension"), times = length(merge_id))
   
 merge_comment_id = data.frame(
-  "Comment on this sample" = merge_comment,
   "Sample_ID" = merge_id)
 
 # write the output files (create repository if necessary)
