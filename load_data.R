59da60acca72620248df8f428f9efbae4d17e7fd
Jean-Noël <jeannoel.lorenzi@gmail.com>
2022-12-05 15:44:17 +0100

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -8,66 +8,62 @@ library("cli")
 opt = config::get(file = paste0(dirname(rstudioapi::getSourceEditorContext()$path), "/config.yml"), config = "default")
 
 # retrieve parameters
-path_data = opt$concatenate_file
-path_id = opt$concatenate_id_file
-path_id_edit = opt$corrected_id_file
-path_output = opt$data_curve
-comment_accepted = gsub(" +$", "", gsub("^ +", "", unlist(strsplit(opt$comment_accepted, ","))))
+# Input
+concatenate_batch_file = opt$concatenate_file
+concatenate_id_file = opt$concatenate_id_file
+concatenate_metadata_file = opt$concatenate_metadata
 
-dir.create(path_output, showWarnings = FALSE)
+# Output
+path_batch_by_id = opt$batch_by_id
+restricted_metadata_file = opt$concatenate_metadata_restricted
 
-file_id = read.table(path_id, sep = "\t", header = T)
-data = read.table(path_data, sep = "\t", header = T)
+#### MAIN
 
-file_id$Comment.on.this.sample = gsub(" +", "_", file_id$Comment.on.this.sample)
+# load data
+metadata = read.table(concatenate_metadata_file, sep = "\t", header = T, check.names = F)
+concatenate_id = read.table(concatenate_id_file, header = T)
 
-#remplacer les commentaires avec des erreurs
-vect_comment = c("cuticle_broked", "cuticule_broke", "no_tape","no_scotch", "two_pupae_too_close")
-vect_comment_corrige = c("cuticle_broke", "cuticle_broke", "no_adhesive_paper","no_adhesive_paper", "two_pupae")
-names(vect_comment_corrige) = vect_comment
-
-for (pb_comment in names(vect_comment_corrige)) {
-  file_id$Comment.on.this.sample[which(file_id$Comment.on.this.sample == pb_comment)] = vect_comment_corrige[pb_comment]
-}
-
-# mofidication des id pour les rendre unique
-list_id = file_id$Sample_ID
-id_count = table(list_id)
-handler_done = c()
-for(id in list_id){
-  if(id_count[id] > 1 & (! id %in% handler_done)){
-    handler_done = c(handler_done, id)
-    vers = 1
-    for(i in which(list_id == id)){
-      list_id[i] = paste(list_id[i], "_", vers, sep = "")
-      vers = vers + 1
+# work only with id present and unique in the metadata and id concatenate file
+list_unique_id = c()
+for (i in 1:length(metadata$Sample_ID)){
+  nb_hit_id_file = length(which(concatenate_id$Sample_ID == metadata$Sample_ID[i]))
+  # check unicity in the id file
+  if (nb_hit_id_file == 1){
+    nb_hit_metadata_file = length(which(metadata$Sample_ID == metadata$Sample_ID[i]))
+    if (nb_hit_metadata_file == 1){
+      list_unique_id = c(list_unique_id, metadata$Sample_ID[i])
     }
   }
 }
 
-file_id$Sample_ID = list_id
+# restrict the data to the selected id
+restricted_metadata = metadata[which(metadata$Sample_ID %in% list_unique_id), ]
 
-dir.create(dirname(path_id_edit), showWarnings = FALSE)
-file_id_edit = file_id[which(file_id$Comment.on.this.sample %in% comment_accepted) , ]
-write.table(file_id_edit, path_id_edit, row.names = F, quote = F, sep = "\t")
+write.table(restricted_metadata, restricted_metadata_file, col.names = T, row.names = F, quote = F, sep = "\t")
+
+# extract data curve from the concatenate batch file
+dir.create(path_batch_by_id, showWarnings = FALSE)
+
+batches = read.table(concatenate_batch_file, sep = "\t", header = T)
 
 pupe_id = 0
-# distinguer chaque id de la table id (3 colonnes par 3 colonnes)
-for (i in seq(1, ncol(data), by = 3)) {# seq permet de prendre une séquence de nombre : seq(nb initial, nb final, pas) ici on prend un pas de 2 pour aller de 2 en 2 sur les colonnes
+# browse the concatenate batch file (by column triplet)
+for (i in seq(1, ncol(batches), by = 3)) {
   pupe_id = pupe_id + 1
-  # filter on comment
-  current_comment = file_id$Comment.on.this.sample[pupe_id]
-  if(! current_comment %in% comment_accepted) next # si le commentaire n'est pas accepte on revient au debut de la boucle
+  # get current id
+  current_id = concatenate_id$Sample_ID[pupe_id]
+  # skip id not present in the restricted metadata file
+  if (! current_id %in% restricted_metadata$Sample_ID) next
   
-  pupe_data = data[, c(i,(i+1), (i+2))]
+  pupe_data = batches[, c(i,(i+1), (i+2))]
   
   colnames(pupe_data) = c("time", "load", "extension")
   
-  # recalibrage de la courbe
+  # recalibration of the curve to 0 on the first 20 load values
   temp = pupe_data$load[1:20]
-  sigma_temp = sd(pupe_data$load[1:20])
+  sigma_temp = sd(pupe_data$load[1:20], na.rm = T)
   
-  # ne pas recalibrer les donnees si le sigma du bruit est inferieur a 0.01
+  # do not recalibrate the data if the noise sigma is less than 0.01
   if (sigma_temp <= 0.01) {
     new_start = which(abs(temp) >= (0.5 * sigma_temp))[1]
     new_index = new_start:length(pupe_data$load)
@@ -80,8 +76,8 @@ for (i in seq(1, ncol(data), by = 3)) {# seq permet de prendre une séquence de 
                            "extension" = pupe_data$extension)
   }
   
-  # ecrire les donnees de la pupe
-  file_path = paste0(path_output, "/", list_id[pupe_id], ".csv")
+  # write pupe data
+  file_path = paste0(path_batch_by_id, "/", current_id, ".csv")
   write.table(pupe_data, file = file_path, quote = F, col.names = T, row.names = F, sep = "\t")
 }
 
