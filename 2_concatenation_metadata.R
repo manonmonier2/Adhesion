43cdd32f3a41d8d1b58644112bf024be0750e4c1
jnlorenzi <70938114+jnlorenzi@users.noreply.github.com>
2024-01-29 14:36:10 +0100

    Update 2_concatenation_metadata.R

--- 2_concatenation_metadata.R
+++ 2_concatenation_metadata.R
@@ -5,7 +5,7 @@ library("config")
 library("dplyr")
 
 # load config file
-opt = config::get(file = paste0(dirname(rstudioapi::getSourceEditorContext()$path), "/config.yml"), config = "manon_acanthoptera")
+opt = config::get(file = paste0(dirname(rstudioapi::getSourceEditorContext()$path), "/config.yml"), config = "portable")
 
 # retrieve parameters
 # Input
